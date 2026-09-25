a8828cb80eb184aa4aadebcba83c20ef8220dfdc
Nishant <nishkohli96@gmail.com>
2026-09-25 14:00:36 +0530

    add mutation route

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
-from app.routers import basic
+from app.routers import basic, mutation
 
-app = FastAPI()
+app = FastAPI(
+  # As docs & redoc heading and tab title
+  title="FastAPI-101",
+  description="Learn",
+)
 app.include_router(basic.router)
+app.include_router(mutation.router)
 
 
 @app.get("/")
---
+++ app/routers/mutation.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Path
+from pydantic import BaseModel, Field
+
+router = APIRouter(prefix="/mutation")
+
+
+class ProductCreate(BaseModel):
+  name: str
+  price: float
+  # optional field
+  category: str | None = None
+  # field with default value
+  stock: int = 1
+
+
+class Supplier(BaseModel):
+  name: str
+  country: str
+
+
+# Apply validation on individual fields
+class UpdateProduct(BaseModel):
+  name: str = Field(min_length=2, max_length=100)
+  price: float = Field(gt=0)
+  stock: int = Field(ge=0)
+  # nested model
+  supplier: Supplier
+
+
+@router.post("/create-product")
+async def create_product(product: ProductCreate):
+  return product
+
+
+@router.post("/update-product/{product_id}")
+async def update_product(
+  product: UpdateProduct,
+  product_id: str = Path(min_length=2),
+):
+  return {
+    "product_id": product_id,
+    "product": product,
+  }
